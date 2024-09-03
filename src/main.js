29f48c8a1efa85c0e1810a603b3bec02654c5bd1
Grigorasch <pgv856@gmail.com>
2024-09-04 00:32:27 +0600

    test

--- src/main.js
+++ src/main.js
@@ -1,26 +1,66 @@
 import plugin from '../plugin.json';
 
 class AcodePlugin {
+  
+  
+  
 
-  async init() {
+  async init() {}
 
-  }
-
-  async destroy() {
-
-  }
+  async destroy() {}
 }
 
 if (window.acode) {
   const acodePlugin = new AcodePlugin();
-  acode.setPluginInit(plugin.id, async (baseUrl, $page, { cacheFileUrl, cacheFile }) => {
-    if (!baseUrl.endsWith('/')) {
-      baseUrl += '/';
-    }
-    acodePlugin.baseUrl = baseUrl;
-    await acodePlugin.init($page, cacheFile, cacheFileUrl);
+  acode.setPluginInit('com.acode.bugger', (baseUrl, $page, cache) => {
+
+window.editorManager.editor.on('guttermousedown', gutterClick);
+
+    
+// TODO and
+    const {
+      commands
+    } = editorManager.editor;
+    commands.addCommand({
+      name: 'example-plugin',
+      bindKey: {
+        win: 'Ctrl-Alt-E', mac: 'Command-Alt-E'
+      },
+      exec: test,
+    });
   });
-  acode.setPluginUnmount(plugin.id, () => {
-    acodePlugin.destroy();
+  acode.setPluginUnmount('com.acode.bugger', () => {
+    const {
+      commands
+    } = editorManager.editor;
+    commands.removeCommand('example-plugin');
+    
+    
+    
+window.editorManager.editor.removeListener('guttermousedown', gutterClick);
+
   });
+}
+
+
+function test() {
+
+const style=document.createElement('style');
+    style.textContent = 
+        '.ace_breakpoint {\
+            border-radius: 20px 0px 0px 20px;\
+            box-shadow: 0px 0px 1px 1px\ #248c46 inset;\
+        }';
+document.head.appendChild(style);
+  window.editorManager.editor.session.setBreakpoint(4);
+  console.log(window);
+  console.log(window.editorManager.editor);
+  
+}
+
+function gutterClick(e) {
+  console.log(document)
+  
+  console.log(document.querySelector('.ace_breakpoint'))
+  console.log(e)
 }
